e5b10577dca396af2e6ce60d0ea3bded3df2e10d
komikodok <komikodok666@gmail.com>
2025-04-09 15:41:33 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+
+    <button type="button" @click="count++" class="btn">count is {{ count }}</button>
+</template>
+
