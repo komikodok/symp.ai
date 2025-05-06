ce0a1f87e7460af440e0198579c13a3ed1c4af94
komikodok <komikodok666@gmail.com>
2025-05-06 18:25:28 +0700

    add Skeleton on SpecialistCard.vue

--- src/components/SpecialistCard.vue
+++ src/components/SpecialistCard.vue
@@ -1,12 +1,57 @@
 <script setup lang="ts">
+import { motion } from 'motion-v';
+import { onMounted } from 'vue';
+
+const { openSkeleton } = defineProps<{
+  openSkeleton?: boolean,
+}>();
+
+const emit = defineEmits<{
+  (e: 'is-open-skeleton', state: boolean): void
+}>();
+
+onMounted(() => {
+  emit('is-open-skeleton', true);
+
+  setTimeout(() => {
+    emit('is-open-skeleton', false);
+  }, 2200)
+});
 </script>
 
 <template>
-  <div class="w-72 bg-white shadow-xl rounded-2xl overflow-hidden">
+  <!-- Skeleton -->
+  <div v-if="openSkeleton" class="w-72 bg-white shadow-xl rounded-2xl overflow-hidden">
+    <div class="flex w-full items-center gap-4 p-4 shadow-md">
+      <div class="w-12 h-12 skeleton rounded-full flex items-center justify-center"></div>
+      <div class="flex flex-col gap-3">
+        <h2 class="skeleton w-42 h-4"></h2>
+        <p class="flex skeleton w-34 h-4 items-center text-sm text-gray-500 gap-1"></p>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-4 px-4 h-42 py-5">
+      <p class="skeleton w-full h-4"></p>
+      <p class="skeleton w-[80%] h-4"></p>
+      <p class="skeleton w-[90%] h-4"></p>
+      <p class="skeleton w-[50%] h-4"></p>
+    </div>
+  </div>
+
+  <!-- Card -->
+  <motion.div 
+    :initial="{ opacity: 0 }"
+    :animate="{ opacity: 1 }"
+    :transition="{
+      duration: 0.8
+    }"
+    v-if="!openSkeleton" 
+    class="w-72 bg-white shadow-xl rounded-2xl overflow-hidden"
+  >
     <!-- Header -->
     <div class="flex items-center gap-4 p-4 shadow-md">
       <div class="w-12 h-12 rounded-full border-2 border-sky-500 flex items-center justify-center">
-        <!-- icon/avatar -->
+        <!-- Avatar -->
       </div>
       <div>
         <h2 class="font-semibold text-lg text-gray-800">Spesialis Jantung</h2>
@@ -40,5 +85,5 @@
         Konsultasi Sekarang
       </button>
     </div>
-  </div>
+  </motion.div>
 </template>
